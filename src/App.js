9aa9685c7c2bd298159080c31d58af7fe65ff76f
Victor Kane <victorkane@gmail.com>
2019-05-10 06:57:28 -0400

    A winning condition is checked for each time board is rendered, and if there is a winner, that winner is shown in status and no further moves (clicks) are allowed.

--- src/App.js
+++ src/App.js
@@ -26,7 +26,30 @@ const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
   const [xIsNext, setXIsNext] = useState(true)
 
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   const handleClick = (i) => {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    } 
     const theSquares = squares.slice();
 		theSquares[i] = 'X';
 		theSquares[i] = xIsNext ? 'X' : 'O';
@@ -43,8 +66,14 @@ const Board = () => {
 		)
   }
 
-	const status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-
+  const winner = calculateWinner(squares);
+	let status;
+	if (winner) {
+	  status = 'Winner: ' + winner;
+	} else {
+	  status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+	}
+  
   return (
     <div>
       <div className="status">{status}</div>
